129140039359015f765fc1d121d8b761ce2f0766
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-05-24 21:34:45 +0900

    🩹遷移先変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:cocoroiki_app/screens/home_screen.dart';
+import 'package:cocoroiki_app/screens/screen.dart';
 import 'package:cocoroiki_app/screens/timeline/post_screen.dart';
+import 'package:cocoroiki_app/screens/timeline/timeline_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -16,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const PostScreen(),
+      home: const Screen(),
     );
   }
 }
