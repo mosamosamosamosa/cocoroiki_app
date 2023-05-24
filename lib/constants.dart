86cd10a716f825273bd23ad368603baf6f378162
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-05-24 16:32:34 +0900

    🎨カラーコードリスト

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// カラー変数
+const kAppBarColor = Color(0xFFF8F8F8);
+const kAppBarFontColor = Color(0xFF000000);
+const kFontColor = Color(0xFF000000);
+const kSubFontColor = Color(0xFF9F9F9F);
+const kImageBoxColor = Color(0xFFD9D9D9);
+const kBorderColor = Color(0xFFCACACA);
+const kNameBoxColor = Color(0xFFEFEFEF);
+const kSelectColor = Color(0xFF777777);
+const kNonSelectColor = Color(0xFF737373);
