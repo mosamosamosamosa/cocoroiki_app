494d559474014222393abbcfdd43158e60978cb2
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-05-27 16:54:47 +0900

    ✨アルバム画面

--- lib/screens/timeline/albam_screen.dart
+++ lib/screens/timeline/albam_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cocoroiki_app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -12,6 +13,28 @@ class AlbamScreen extends StatefulWidget {
 class _AlbamScreenState extends State<AlbamScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double deviceW = MediaQuery.of(context).size.width;
+    double deviceH = MediaQuery.of(context).size.height;
+
+    List<Widget> imageList = [
+      Container(width: 120, height: 120, color: kImageBoxColor),
+      Container(width: 120, height: 120, color: kImageBoxColor)
+    ];
+    return Scaffold(
+        body: SizedBox(
+            height: deviceH * 0.7,
+            width: deviceW,
+            child: GridView.count(
+                //childAspectRatio: 1.2,
+                mainAxisSpacing: 7,
+                crossAxisSpacing: 7,
+                crossAxisCount: 3,
+                children: List.generate(
+                    11,
+                    (index) => Container(
+                          width: 125,
+                          height: 125,
+                          color: kImageBoxColor,
+                        )))));
   }
 }
