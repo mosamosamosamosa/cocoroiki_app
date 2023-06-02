359afad1097ab428b0a8628a9039bbebfe13b6ce
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-06-02 14:50:02 +0900

    ✨クエスト広場作成

--- lib/screens/quest/quest_screen.dart
+++ lib/screens/quest/quest_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cocoroiki_app/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class QuestScreen extends StatefulWidget {
   const QuestScreen({super.key});
@@ -9,6 +10,27 @@ class QuestScreen extends StatefulWidget {
 }
 
 class _QuestScreenState extends State<QuestScreen> {
+  int granCount = 4;
+  bool visible1 = false;
+  bool visible2 = false;
+  bool visible3 = false;
+  bool visible4 = false;
+
+  @override
+  void initState() {
+    if (granCount == 1) {
+      visible1 = true;
+    } else if (granCount == 2) {
+      visible2 = true;
+    } else if (granCount == 3) {
+      visible3 = true;
+    } else {
+      visible4 = true;
+    }
+    setState(() {});
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -26,9 +48,74 @@ class _QuestScreenState extends State<QuestScreen> {
         child: Scaffold(
             backgroundColor: Colors.transparent,
             body: Stack(
+              alignment: Alignment.bottomCenter,
               children: [
-                Positioned(
-                    bottom: 0, child: Image.asset('assets/image/back.png'))
+                Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Image.asset('assets/image/back.png')),
+                Visibility(
+                  visible: visible1,
+                  child: Positioned(
+                      bottom: 260,
+                      child: SvgPicture.asset('assets/svg/green.svg')),
+                ),
+                Visibility(
+                  visible: visible2,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                          left: 50,
+                          bottom: 220,
+                          child: SvgPicture.asset('assets/svg/green.svg')),
+                      Positioned(
+                          right: 60,
+                          bottom: 300,
+                          child: SvgPicture.asset('assets/svg/green.svg')),
+                    ],
+                  ),
+                ),
+                Visibility(
+                  visible: visible3,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                          left: 50,
+                          bottom: 200,
+                          child: SvgPicture.asset('assets/svg/green.svg')),
+                      Positioned(
+                          right: 40,
+                          bottom: 280,
+                          child: SvgPicture.asset('assets/svg/green.svg')),
+                      Positioned(
+                          left: 70,
+                          bottom: 370,
+                          child: SvgPicture.asset('assets/svg/green.svg')),
+                    ],
+                  ),
+                ),
+                Visibility(
+                  visible: visible4,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                          left: 50,
+                          bottom: 400,
+                          child: SvgPicture.asset('assets/svg/green.svg')),
+                      Positioned(
+                          right: 60,
+                          bottom: 330,
+                          child: SvgPicture.asset('assets/svg/green.svg')),
+                      Positioned(
+                          left: 40,
+                          bottom: 220,
+                          child: SvgPicture.asset('assets/svg/green.svg')),
+                      Positioned(
+                          right: 20,
+                          bottom: 180,
+                          child: SvgPicture.asset('assets/svg/green.svg')),
+                    ],
+                  ),
+                ),
               ],
             )));
   }
