70e552fb805f4c2804d2c310b718e22a4667588c
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-06-08 20:03:58 +0900

    🩹遷移先変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:cocoroiki_app/screens/kid/quest/quest_screen.dart';
+import 'package:cocoroiki_app/screens/kid/timelinekids.dart';
 import 'package:cocoroiki_app/screens/screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Screen(),
+      home: QuestScreen(),
     );
   }
 }
