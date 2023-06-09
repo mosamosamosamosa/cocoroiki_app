923918e12801ca8e197a734ad7f9d7852ca62f6f
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-06-09 11:58:03 +0900

    🩹画像表示モーダルへの遷移

--- lib/components/post_image_four.dart
+++ lib/components/post_image_four.dart
@@ -1,5 +1,6 @@
 import 'package:cocoroiki_app/constants.dart';
 import 'package:cocoroiki_app/data/database.dart';
+import 'package:cocoroiki_app/screens/image_modal.dart';
 import 'package:flutter/material.dart';
 
 class PostImageFour extends StatefulWidget {
@@ -17,64 +18,96 @@ class _PostImageFourState extends State<PostImageFour> {
         SizedBox(width: 22),
         Column(
           children: [
-            Container(
-              clipBehavior: Clip.antiAlias,
-              height: 96,
-              width: 152,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(10),
+            GestureDetector(
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) =>
+                        ImageModal(image: posts_list[0].image_url[0]));
+              },
+              child: Container(
+                clipBehavior: Clip.antiAlias,
+                height: 96,
+                width: 152,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                  ),
                 ),
+                child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: Image.network(posts_list[0].image_url[0])),
               ),
-              child: FittedBox(
-                  fit: BoxFit.cover,
-                  child: Image.network(posts_list[0].image_url[0])),
             ),
             SizedBox(height: 8),
-            Container(
-              clipBehavior: Clip.antiAlias,
-              height: 96,
-              width: 152,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(10),
+            GestureDetector(
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) =>
+                        ImageModal(image: posts_list[0].image_url[0]));
+              },
+              child: Container(
+                clipBehavior: Clip.antiAlias,
+                height: 96,
+                width: 152,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                  ),
                 ),
+                child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: Image.network(posts_list[0].image_url[0])),
               ),
-              child: FittedBox(
-                  fit: BoxFit.cover,
-                  child: Image.network(posts_list[0].image_url[0])),
             ),
           ],
         ),
         SizedBox(width: 8),
         Column(
           children: [
-            Container(
-              clipBehavior: Clip.antiAlias,
-              height: 96,
-              width: 152,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(10),
+            GestureDetector(
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) =>
+                        ImageModal(image: posts_list[0].image_url[0]));
+              },
+              child: Container(
+                clipBehavior: Clip.antiAlias,
+                height: 96,
+                width: 152,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(10),
+                  ),
                 ),
+                child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: Image.network(posts_list[0].image_url[0])),
               ),
-              child: FittedBox(
-                  fit: BoxFit.cover,
-                  child: Image.network(posts_list[0].image_url[0])),
             ),
             SizedBox(height: 8),
-            Container(
-              clipBehavior: Clip.antiAlias,
-              height: 96,
-              width: 152,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(10),
+            GestureDetector(
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) =>
+                        ImageModal(image: posts_list[0].image_url[0]));
+              },
+              child: Container(
+                clipBehavior: Clip.antiAlias,
+                height: 96,
+                width: 152,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(10),
+                  ),
                 ),
+                child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: Image.network(posts_list[0].image_url[0])),
               ),
-              child: FittedBox(
-                  fit: BoxFit.cover,
-                  child: Image.network(posts_list[0].image_url[0])),
             ),
           ],
         ),
--- lib/components/post_image_one.dart
+++ lib/components/post_image_one.dart
@@ -1,5 +1,6 @@
 import 'package:cocoroiki_app/constants.dart';
 import 'package:cocoroiki_app/data/database.dart';
+import 'package:cocoroiki_app/screens/image_modal.dart';
 import 'package:flutter/material.dart';
 
 class PostImageOne extends StatefulWidget {
@@ -12,17 +13,26 @@ class PostImageOne extends StatefulWidget {
 class _PostImageOneState extends State<PostImageOne> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.antiAlias,
-      height: 200,
-      width: 300,
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.only(
-          topRight: Radius.circular(10),
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) =>
+                ImageModal(image: posts_list[0].image_url[0]));
+      },
+      child: Container(
+        clipBehavior: Clip.antiAlias,
+        height: 200,
+        width: 300,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
         ),
+        child: FittedBox(
+            fit: BoxFit.cover,
+            child: Image.network(posts_list[0].image_url[0])),
       ),
-      child: FittedBox(
-          fit: BoxFit.cover, child: Image.network(posts_list[0].image_url[0])),
     );
   }
 }
