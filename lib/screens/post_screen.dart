fb3fc93cc8d2c08cd2667cad8c590a4ceb317be6
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-06-16 15:56:10 +0900

    ✨カメラ・ライブラリへのアクセスメソッド

--- lib/screens/post_screen.dart
+++ lib/screens/post_screen.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:cocoroiki_app/components/custom_app_bar.dart';
 
 import 'package:cocoroiki_app/constants.dart';
 import 'package:cocoroiki_app/data/database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PostScreen extends StatefulWidget {
   const PostScreen({super.key});
@@ -14,8 +18,45 @@ class PostScreen extends StatefulWidget {
 
 class _PostScreenState extends State<PostScreen> {
   final tags = ['まもる', 'かおり', 'ゆうた'];
-  final images = [];
+  final List<File> images = [];
   var selectedTags = <String>[];
+
+  File? image;
+  ImagePicker picker = ImagePicker();
+
+  //IOSのみ許可の記述がいる
+  //フォルダから選んだ画像を読みとる
+  Future pickImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      // 画像がnullの場合戻る
+      if (image == null) return;
+
+      final imageTemp = File(image.path);
+
+      setState(() {
+        this.image = imageTemp;
+        images.add(imageTemp);
+      });
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  // カメラを使う関数
+  Future pickImageC() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.camera);
+      // 画像がnullの場合戻る
+      if (image == null) return;
+
+      final imageTemp = File(image.path);
+
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -76,80 +117,60 @@ class _PostScreenState extends State<PostScreen> {
                           border: InputBorder.none, hintText: '今日の出来事を共有しましょう'),
                     ),
                   ),
-                  SizedBox(height: 24),
-                  images.length == 0
-                      ? Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            SizedBox(width: 10),
-                            Container(
-                                alignment: Alignment.center,
-                                height: 208,
-                                width: 342,
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
+                  Stack(
+                    children: [
+                      Column(
+                        children: [
+                          SizedBox(height: 24),
+                          images.length == 0
+                              ? Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
                                   children: [
-                                    SvgPicture.asset(
-                                        'assets/svg/image_icon.svg'),
-                                    Text("写真を1〜4枚追加しましょう",
-                                        style: TextStyle(
-                                            fontSize: 14,
-                                            color: kSubFontColor)),
-                                  ],
-                                ),
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(10),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.black26, //色
+                                    SizedBox(width: 10),
+                                    GestureDetector(
+                                      onTap: () {
+                                        print('タップ');
+                                        pickImage();
+                                      },
+                                      child: Container(
+                                          alignment: Alignment.center,
+                                          height: 208,
+                                          width: 342,
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              SvgPicture.asset(
+                                                  'assets/svg/image_icon.svg'),
+                                              Text("写真を1〜4枚追加しましょう",
+                                                  style: TextStyle(
+                                                      fontSize: 14,
+                                                      color: kSubFontColor)),
+                                            ],
+                                          ),
+                                          decoration: BoxDecoration(
+                                            color: Colors.white,
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            boxShadow: [
+                                              BoxShadow(
+                                                color: Colors.black26, //色
 
-                                      offset: Offset(0, 2),
+                                                offset: Offset(0, 2),
+                                              ),
+                                            ],
+                                          )),
                                     ),
+                                    SizedBox(width: 10),
                                   ],
-                                )),
-                            SizedBox(width: 10),
-                          ],
-                        )
-                      : images.length == 1
-                          ? Padding(
-                              padding: const EdgeInsets.only(left: 24.0),
-                              child: Stack(
-                                children: [
-                                  Container(
-                                    clipBehavior: Clip.antiAlias,
-                                    height: 208,
-                                    width: 342,
-                                    decoration: const BoxDecoration(
-                                      borderRadius: BorderRadius.all(
-                                        Radius.circular(10),
-                                      ),
-                                    ),
-                                    child: FittedBox(
-                                        fit: BoxFit.cover,
-                                        child: Image.network(
-                                            posts_list[0].image_url[0])),
-                                  ),
-                                  Positioned(
-                                      right: 12,
-                                      top: 12,
-                                      child: SvgPicture.asset(
-                                          'assets/svg/image_batsu.svg'))
-                                ],
-                              ),
-                            )
-                          : SizedBox(
-                              height: 208,
-                              width: deviceW,
-                              child: ListView.builder(
-                                itemCount: images.length,
-                                scrollDirection: Axis.horizontal,
-                                itemBuilder: (BuildContext context, int index) {
-                                  return Stack(
-                                    children: [
-                                      Row(
+                                )
+                              : images.length == 1
+                                  ? Padding(
+                                      padding:
+                                          const EdgeInsets.only(left: 24.0),
+                                      child: Stack(
                                         children: [
-                                          SizedBox(width: 8),
                                           Container(
                                             clipBehavior: Clip.antiAlias,
                                             height: 208,
@@ -161,25 +182,91 @@ class _PostScreenState extends State<PostScreen> {
                                             ),
                                             child: FittedBox(
                                                 fit: BoxFit.cover,
-                                                child: Image.network(
-                                                    posts_list[0]
-                                                        .image_url[0])),
+                                                child: Image.file(images[0])),
                                           ),
+                                          Positioned(
+                                              right: 12,
+                                              top: 12,
+                                              child: GestureDetector(
+                                                onTap: () {
+                                                  print(images);
+                                                  setState(() {
+                                                    images.removeAt(0);
+                                                  });
+                                                },
+                                                child: SvgPicture.asset(
+                                                    'assets/svg/image_batsu.svg'),
+                                              ))
                                         ],
                                       ),
-                                      Positioned(
-                                          right: 12,
-                                          top: 12,
-                                          child: SvgPicture.asset(
-                                              'assets/svg/image_batsu.svg'))
-                                    ],
-                                  );
-                                },
-                              ),
-                            )
+                                    )
+                                  : SizedBox(
+                                      height: 208,
+                                      width: deviceW,
+                                      child: ListView.builder(
+                                        itemCount: images.length,
+                                        scrollDirection: Axis.horizontal,
+                                        itemBuilder:
+                                            (BuildContext context, int index) {
+                                          return Stack(
+                                            children: [
+                                              Row(
+                                                children: [
+                                                  SizedBox(width: 8),
+                                                  Container(
+                                                    clipBehavior:
+                                                        Clip.antiAlias,
+                                                    height: 208,
+                                                    width: 342,
+                                                    decoration:
+                                                        const BoxDecoration(
+                                                      borderRadius:
+                                                          BorderRadius.all(
+                                                        Radius.circular(10),
+                                                      ),
+                                                    ),
+                                                    child: FittedBox(
+                                                        fit: BoxFit.cover,
+                                                        child: Image.file(
+                                                            images[index])),
+                                                  ),
+                                                ],
+                                              ),
+                                              Positioned(
+                                                  right: 12,
+                                                  top: 12,
+                                                  child: GestureDetector(
+                                                    onTap: () {
+                                                      setState(() {
+                                                        images.removeAt(index);
+                                                      });
+                                                    },
+                                                    child: SvgPicture.asset(
+                                                        'assets/svg/image_batsu.svg'),
+                                                  )),
+                                            ],
+                                          );
+                                        },
+                                      ),
+                                    ),
+                          SizedBox(height: 40),
+                        ],
+                      ),
+                      Positioned(
+                          bottom: 10,
+                          right: 0,
+                          child: GestureDetector(
+                              onTap: () {
+                                pickImage();
+                              },
+                              child: images.length == 0
+                                  ? Container()
+                                  : SvgPicture.asset(
+                                      'assets/svg/image_add.svg')))
+                    ],
+                  )
                 ],
               ),
-              SizedBox(height: 40),
               Divider(
                 color: kBorderColor,
                 thickness: 0.5,
