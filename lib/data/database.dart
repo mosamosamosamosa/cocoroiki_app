9082a0ed86bc9a042ef964299b1561ca4c212bad
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-07-25 12:56:46 +0900

    🩹仮database

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -57,5 +57,10 @@ List<Users> users_list = [
       id: 1,
       name: '朋子',
       image:
-          'https://thumb.photo-ac.com/cd/cd1c7ccec442b51fb3de581e0c0e5cb0_t.jpeg')
+          'https://thumb.photo-ac.com/11/11f9fa9424c0ec309bbb18139bfd1a82_w.jpeg'),
+  Users(
+      id: 2,
+      name: 'よしえ',
+      image:
+          'https://thumb.photo-ac.com/0f/0fe47e6740d3e2339ce2abb0e8dead64_w.jpeg')
 ];
