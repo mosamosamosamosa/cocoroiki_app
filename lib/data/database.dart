7420416003660ec721c3c6da2ff3d4724e816a12
櫻井 雛羽 <2200496@ecc.ac.jp>
2023-08-29 02:31:12 +0900

    🪠仮データベース

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -64,3 +64,105 @@ List<Users> users_list = [
       image:
           'https://thumb.photo-ac.com/0f/0fe47e6740d3e2339ce2abb0e8dead64_w.jpeg')
 ];
+
+class QuestManage {
+  final String date;
+  final String title;
+  final String icon;
+  final String gohoubi;
+
+  QuestManage(
+      {required this.date,
+      required this.title,
+      required this.icon,
+      required this.gohoubi});
+}
+
+List<QuestManage> quest_manage = [
+  QuestManage(
+      date: '2023年8月28日',
+      title: 'ごはんをつくろう',
+      icon: 'gohoubi_kodukai_icon.svg',
+      gohoubi: '10円ゲット!'),
+  QuestManage(
+      date: '2023年8月34日',
+      title: 'おさんぽしよう',
+      icon: 'gohoubi_omocha_icon.svg',
+      gohoubi: 'ミニカーゲット!'),
+  QuestManage(
+      date: '2023年5月23日',
+      title: 'おりょうりしよう',
+      icon: 'gohoubi_kodukai_icon.svg',
+      gohoubi: '10円ゲット!'),
+];
+
+class Reward1 {
+  final String title;
+  final String icon;
+
+  Reward1({
+    required this.title,
+    required this.icon,
+  });
+}
+
+List<Reward1> reward1_list = [
+  Reward1(
+    title: '10円',
+    icon: 'gohoubi_kodukai_icon.svg',
+  ),
+  Reward1(
+    title: 'あめ',
+    icon: 'gohoubi_okashi_icon.svg',
+  ),
+];
+
+class Reward2 {
+  final String title;
+  final String icon;
+
+  Reward2({
+    required this.title,
+    required this.icon,
+  });
+}
+
+List<Reward2> reward2_list = [
+  Reward2(
+    title: '50円',
+    icon: 'gohoubi_kodukai_icon.svg',
+  ),
+  Reward2(
+    title: 'クッキー',
+    icon: 'gohoubi_sweet_icon.svg',
+  ),
+  Reward2(
+    title: 'アイス',
+    icon: 'gohoubi_okashi_icon.svg',
+  ),
+];
+
+class Reward3 {
+  final String title;
+  final String icon;
+
+  Reward3({
+    required this.title,
+    required this.icon,
+  });
+}
+
+List<Reward3> reward3_list = [
+  Reward3(
+    title: '100円',
+    icon: 'gohoubi_kodukai_icon.svg',
+  ),
+  Reward3(
+    title: 'ケーキ',
+    icon: 'gohoubi_okashi_icon.svg',
+  ),
+  Reward3(
+    title: 'ミニカー',
+    icon: 'gohoubi_omocha_icon.svg',
+  ),
+];
